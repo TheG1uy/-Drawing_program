484c88c325d75f11bafc66b541b64896d5f57acf
TheG1uy <paulnvidia@gmail.com>
2018-04-20 21:09:09 +0300

    Program_complete

--- Drawing_program/Drawing_program/Drawing_program.h
+++ Drawing_program/Drawing_program/Drawing_program.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Figures.h"
+#include  <string>
 namespace Drawing_program {
 
 	using namespace System;
@@ -22,7 +23,13 @@ namespace Drawing_program {
 		TRoot *pCurr;
 		int i;
 	private: System::Windows::Forms::CheckBox^  checkBox1;
-	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^  actionToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  saveToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  openToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  deleteLineToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  refreshToolStripMenuItem;
+
 	public:
 		TFind *find;
 		Drawing_program(void)
@@ -65,7 +72,13 @@ namespace Drawing_program {
 		void InitializeComponent(void)
 		{
 			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->actionToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->saveToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->openToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->deleteLineToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->refreshToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// checkBox1
@@ -73,22 +86,58 @@ namespace Drawing_program {
 			this->checkBox1->AutoSize = true;
 			this->checkBox1->Location = System::Drawing::Point(586, 2);
 			this->checkBox1->Name = L"checkBox1";
-			this->checkBox1->Size = System::Drawing::Size(81, 17);
+			this->checkBox1->Size = System::Drawing::Size(88, 17);
 			this->checkBox1->TabIndex = 0;
-			this->checkBox1->Text = L"GOD_MOD";
+			this->checkBox1->Text = L"GOD_MODE";
 			this->checkBox1->UseVisualStyleBackColor = true;
 			this->checkBox1->CheckedChanged += gcnew System::EventHandler(this, &Drawing_program::checkBox1_CheckedChanged);
 			// 
-			// button1
+			// menuStrip1
 			// 
-			this->button1->Location = System::Drawing::Point(586, 25);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(80, 22);
-			this->button1->TabIndex = 1;
-			this->button1->Text = L"Delete";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Visible = false;
-			this->button1->Click += gcnew System::EventHandler(this, &Drawing_program::button1_Click);
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->actionToolStripMenuItem });
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(678, 24);
+			this->menuStrip1->TabIndex = 2;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// actionToolStripMenuItem
+			// 
+			this->actionToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(4) {
+				this->saveToolStripMenuItem,
+					this->openToolStripMenuItem, this->deleteLineToolStripMenuItem, this->refreshToolStripMenuItem
+			});
+			this->actionToolStripMenuItem->Name = L"actionToolStripMenuItem";
+			this->actionToolStripMenuItem->Size = System::Drawing::Size(54, 20);
+			this->actionToolStripMenuItem->Text = L"Action";
+			// 
+			// saveToolStripMenuItem
+			// 
+			this->saveToolStripMenuItem->Name = L"saveToolStripMenuItem";
+			this->saveToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->saveToolStripMenuItem->Text = L"Save";
+			this->saveToolStripMenuItem->Click += gcnew System::EventHandler(this, &Drawing_program::saveToolStripMenuItem_Click);
+			// 
+			// openToolStripMenuItem
+			// 
+			this->openToolStripMenuItem->Name = L"openToolStripMenuItem";
+			this->openToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->openToolStripMenuItem->Text = L"Open";
+			this->openToolStripMenuItem->Click += gcnew System::EventHandler(this, &Drawing_program::openToolStripMenuItem_Click);
+			// 
+			// deleteLineToolStripMenuItem
+			// 
+			this->deleteLineToolStripMenuItem->Name = L"deleteLineToolStripMenuItem";
+			this->deleteLineToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->deleteLineToolStripMenuItem->Text = L"Delete_Line";
+			this->deleteLineToolStripMenuItem->Click += gcnew System::EventHandler(this, &Drawing_program::deleteLineToolStripMenuItem_Click);
+			// 
+			// refreshToolStripMenuItem
+			// 
+			this->refreshToolStripMenuItem->Name = L"refreshToolStripMenuItem";
+			this->refreshToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->refreshToolStripMenuItem->Text = L"Refresh";
+			this->refreshToolStripMenuItem->Click += gcnew System::EventHandler(this, &Drawing_program::refreshToolStripMenuItem_Click);
 			// 
 			// Drawing_program
 			// 
@@ -96,19 +145,31 @@ namespace Drawing_program {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::White;
 			this->ClientSize = System::Drawing::Size(678, 458);
-			this->Controls->Add(this->button1);
 			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"Drawing_program";
 			this->Text = L"Drawing_program";
+			this->Load += gcnew System::EventHandler(this, &Drawing_program::Drawing_program_Load);
 			this->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &Drawing_program::Drawing_program_MouseDown);
 			this->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &Drawing_program::Drawing_program_MouseMove);
 			this->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &Drawing_program::Drawing_program_MouseUp);
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	
+		string MarshalString(String ^ s) {
+			string os;
+			using namespace Runtime::InteropServices;
+			const char* chars =
+				(const char*)(Marshal::StringToHGlobalAnsi(s)).ToPointer();
+			os = chars;
+			Marshal::FreeHGlobal(IntPtr((void*)chars));
+			return os;
+		}
 	private: System::Void Drawing_program_MouseDown(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
 		if (!checkBox1->Checked){
 			x1 = x2 = e->X;
@@ -132,10 +193,13 @@ namespace Drawing_program {
 
 			else {
 				*find = pFirst->Find(x1, x2, y1, y2);
-				if (find->pS != nullptr && find->pS->visible == false && find->pE != nullptr && find->pE->visible == false)  find->pE->visible == true;
+				if (find->pS != nullptr && find->pS->visible == false && find->pE != nullptr && find->pE->visible == false && !(pFirst->Distance(x2, y2) || pFirst->Distance(x1, y1))) {
+					find->pS = nullptr;
+					find->pE = nullptr;
+				}
 				else {
-					if (find->pS != nullptr && find->pS->visible == false) find->pS = nullptr;
-					if (find->pE != nullptr && find->pE->visible == false) find->pE = nullptr;
+					if (find->pS != nullptr && find->pS->visible == false && find->pE == nullptr && !(pFirst->Distance(x2,y2) || pFirst->Distance(x1, y1))) find->pS = nullptr;
+					if (find->pE != nullptr && find->pE->visible == false && find->pS == nullptr && !(pFirst->Distance(x2, y2) || pFirst->Distance(x1, y1))) find->pE = nullptr;
 				}
 				if (find->pS == nullptr && find->pE == nullptr) {
 					gr->Clear(Color::White);
@@ -196,21 +260,57 @@ namespace Drawing_program {
 	private: System::Void checkBox1_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
 		
 		
-		if (checkBox1->Checked) { if (pFirst != nullptr) pFirst->Draw(gr, Color::AntiqueWhite); button1->Visible = true; }
+		if (checkBox1->Checked) { if (pFirst != nullptr) { pFirst->Draw(gr, Color::Bisque); deleteLineToolStripMenuItem->Enabled = true; } }
 			
-		else if (pFirst != nullptr) {
-			pFirst->Draw(gr, Color::Black);
-			button1->Visible = false;
+		else {
+			if (pFirst != nullptr) {
+				pFirst->Draw(gr, Color::Black);
+			}
+			deleteLineToolStripMenuItem->Enabled = false;
 		}
+		
 		
 	}
 private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	
+}
+private: System::Void deleteLineToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
 	if (pCurr != nullptr) {
 		pCurr->visible = false;
 		gr->Clear(Color::White);
 		pFirst->Draw(gr, Color::AntiqueWhite);
 		pCurr = nullptr;
 	}
+}
+
+private: System::Void Drawing_program_Load(System::Object^  sender, System::EventArgs^  e) {
+	deleteLineToolStripMenuItem->Enabled = false;
+}
+private: System::Void saveToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+	FILE *f;
+	f = fopen("text.txt", "r");
+	pFirst->Get_Points("text.txt");
+
+}
+private: System::Void openToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+	FILE *f;
+	//f = fopen("text.txt", "r");
+	OpenFileDialog^ dialog = gcnew OpenFileDialog();
+	dialog->Filter = "Text files | *.txt; | All Files (*.*) | *.*";
+	if (dialog->ShowDialog() == System::Windows::Forms::DialogResult::OK)
+	{
+	  f=fopen(MarshalString(dialog->FileName).c_str(),"r");
+	  pFirst = pFirst->CreateFigure(f);
+	  gr->Clear(Color::White);
+	  pFirst->Draw(gr,Color::Black);
+	}
+}
+private: System::Void refreshToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+	if (pFirst != nullptr) {
+		pFirst->ClearChart();
+		pFirst = nullptr;
+	}
+	gr->Clear(Color::White);
 }
 };
 }
--- Drawing_program/Drawing_program/Figures.h
+++ Drawing_program/Drawing_program/Figures.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "TStack.h"
+#include "queue.h"
 #include <math.h>
-
+#include <fstream> 
 using namespace std;
 using namespace System::Drawing;
 
@@ -233,7 +234,236 @@ public:
 		return find;
 	
 	}
-	
+	void ClearChart() {
+		TStack<TCurrLine> st;
+		TCurrLine curr;
+		curr.tC = this;
+		curr.pS = curr.pE = nullptr;
+		st.clear();
+		stack.clear();
+		stack.push(curr);
+		while (!stack.isEmpty()) {
+			curr = stack.pop();
+			while (curr.pS == nullptr) {
+				if (dynamic_cast<TChart *>(curr.tC)->pStart->whatIsIt == 1)
+					curr.pS = dynamic_cast<TChart *>(curr.tC)->pStart;
+				else {
+					stack.push(curr);
+					curr.tC = dynamic_cast<TChart *>(curr.tC)->pStart;
+				}
+			}
+			if (curr.pE == nullptr) {
+				if (dynamic_cast<TChart *>(curr.tC)->pEnd->whatIsIt == 1)
+					curr.pE = dynamic_cast<TChart *>(curr.tC)->pEnd;
+				else {
+
+					stack.push(curr);
+					curr.tC = dynamic_cast<TChart *>(curr.tC)->pEnd;
+					curr.pS = nullptr;
+					stack.push(curr);
+				}
+			}
+			if (curr.pS != nullptr && curr.pE != nullptr)
+				if (curr.pS->whatIsIt == 1 && curr.pE->whatIsIt == 1) {
+					TPoint *p1 = dynamic_cast<TPoint *>(curr.pS), *p2 = dynamic_cast<TPoint *>(curr.pE);
+					st.push(curr);
+					if (!stack.isEmpty()) {
+						curr = stack.pop();
+						if (curr.pS == nullptr) curr.pS = (TRoot*)p2;
+						else curr.pE = (TRoot*)p2;
+						stack.push(curr);
+					}
+				}
+
+		}
+		
+		while (!st.isEmpty()) {
+			curr = st.pop();
+			delete curr.tC;
+			//delete curr.pE;
+			//delete curr.pS;
+			//curr.tC = nullptr;
+			//curr.pE = nullptr;
+		//	curr.pS = nullptr;
+		}
+	}
+	TChart* CreateFigure(FILE *fn) {
+		TStack<int> std;
+		TQueue<TChart *> queue;
+		ifstream file(fn);
+		TFind *find=new TFind();
+		char buf[80];
+		TChart *tFirst = nullptr;
+		std.clear();
+		while (!file.eof()) {
+			TChart *tmp;
+			TPoint  *p1 = new TPoint() , *p2 = new TPoint();
+			file.getline(buf, 80, '\n');
+			if (buf[0] == '\0') break;
+			string tmpS;
+			for (int i = 0; buf[i]!='\0'; i++) {
+				if (buf[i] >= '0' && buf[i] <= '9') {
+					tmpS += buf[i];
+				}
+				else if (tmpS.size() > 0) {
+					std.push(atof(tmpS.c_str()));
+					tmpS = "";
+				}
+			}
+	        
+			p2->setY(std.pop());
+			p2->setX(std.pop());
+			p1->setY(std.pop());
+			p1->setX(std.pop());
+			if (tFirst == nullptr) {
+				tFirst = new TChart(p1, p2);
+			}
+			else {
+				tmp = new TChart(p1, p2);
+				queue.push(tmp);
+			}
+		}
+		while (!queue.isempty()) {
+			TChart *tmp = queue.pop();
+			int x1 = dynamic_cast<TPoint *>(tmp->getBegin())->getX(), y1 = dynamic_cast<TPoint *>(tmp->getBegin())->getY();
+			int x2 = dynamic_cast<TPoint *>(tmp->getEnd())->getX(), y2 = dynamic_cast<TPoint *>(tmp->getEnd())->getY();
+			*find = tFirst->Find(x1, x2, y1, y2);
+			if (find->pS == nullptr && find->pE == nullptr) {
+				queue.push(tmp);
+				continue;
+			}
+		
+
+			if (find->pS != nullptr && find->pE != nullptr) {
+				TPoint  *p1, *p2;
+				if (find->Start == 1) p1 = dynamic_cast<TPoint*>(dynamic_cast<TChart*>(find->pS)->getBegin());
+				if (find->Start == 2) p1 = dynamic_cast<TPoint*>(dynamic_cast<TChart*>(find->pS)->getEnd());
+				if (find->End == 1) p2 = dynamic_cast<TPoint*>(dynamic_cast<TChart*>(find->pE)->getBegin());
+				if (find->End == 2) p2 = dynamic_cast<TPoint*>(dynamic_cast<TChart*>(find->pE)->getEnd());
+				if (tFirst->CopyrightedTest(p1->getX(), p1->getY(), p2->getX(), p2->getY())) {
+					TChart *tmp = new TChart(p1, p2);
+					if (find->End == 1) dynamic_cast<TChart*>(find->pE)->setStart(tmp);
+					if (find->End == 2) dynamic_cast<TChart*>(find->pE)->setEnd(tmp);
+
+				}
+				continue;
+			}
+			if (find->pS != nullptr) {
+				TPoint  *p1, *p2 = new TPoint(x2, y2);
+				if (find->Start == 1) p1 = dynamic_cast<TPoint*>(dynamic_cast<TChart*>(find->pS)->getBegin());
+				if (find->Start == 2) p1 = dynamic_cast<TPoint*>(dynamic_cast<TChart*>(find->pS)->getEnd());
+				TChart *tmp = new TChart(p2, p1);
+				if (find->Start == 1) dynamic_cast<TChart*>(find->pS)->setStart(tmp);
+				if (find->Start == 2) dynamic_cast<TChart*>(find->pS)->setEnd(tmp);
+			}
+			if (find->pE != nullptr) {
+				TPoint  *p1 = new TPoint(x1, y1), *p2;
+				if (find->End == 1) p2 = dynamic_cast<TPoint*>(dynamic_cast<TChart*>(find->pE)->getBegin());
+				if (find->End == 2) p2 = dynamic_cast<TPoint*>(dynamic_cast<TChart*>(find->pE)->getEnd());
+				TChart *tmp = new TChart(p1, p2);
+				if (find->End == 1) dynamic_cast<TChart*>(find->pE)->setStart(tmp);
+				if (find->End == 2) dynamic_cast<TChart*>(find->pE)->setEnd(tmp);
+			}
+		}
+		return tFirst;
+	}
+	void Get_Points(char* fn) {
+		ofstream ofs(fn);
+		TCurrLine curr;
+		curr.tC = this;
+		curr.pS = curr.pE = nullptr;
+		stack.clear();
+		stack.push(curr);
+		while (!stack.isEmpty()) {
+			curr = stack.pop();
+			while (curr.pS == nullptr) {
+				if (dynamic_cast<TChart *>(curr.tC)->pStart->whatIsIt == 1)
+					curr.pS = dynamic_cast<TChart *>(curr.tC)->pStart;
+				else {
+					stack.push(curr);
+					curr.tC = dynamic_cast<TChart *>(curr.tC)->pStart;
+				}
+			}
+			if (curr.pE == nullptr) {
+				if (dynamic_cast<TChart *>(curr.tC)->pEnd->whatIsIt == 1)
+					curr.pE = dynamic_cast<TChart *>(curr.tC)->pEnd;
+				else {
+
+					stack.push(curr);
+					curr.tC = dynamic_cast<TChart *>(curr.tC)->pEnd;
+					curr.pS = nullptr;
+					stack.push(curr);
+				}
+			}
+			if (curr.pS != nullptr && curr.pE != nullptr)
+				if (curr.pS->whatIsIt == 1 && curr.pE->whatIsIt == 1) {
+					TPoint *p1 = dynamic_cast<TPoint *>(curr.pS), *p2 = dynamic_cast<TPoint *>(curr.pE);
+					if (curr.tC->visible)
+					ofs << "("<<p1->getX()<<","<<p1->getY()<<")" <<" , ("<< p2->getX()<<","<<p2->getY()<<")"<< endl;
+					if (!stack.isEmpty()) {
+						curr = stack.pop();
+						if (curr.pS == nullptr) curr.pS = (TRoot*)p2;
+						else curr.pE = (TRoot*)p2;
+						stack.push(curr);
+					}
+				}
+
+		}
+	}
+	bool Distance(int x1, int y1) {
+		int Epsilon = 10;
+		TCurrLine curr;
+		curr.tC = this;
+		curr.pS = curr.pE = nullptr;
+		stack.clear();
+		stack.push(curr);
+		while (!stack.isEmpty()) {
+			curr = stack.pop();
+			while (curr.pS == nullptr) {
+				if (dynamic_cast<TChart *>(curr.tC)->pStart->whatIsIt == 1)
+					curr.pS = dynamic_cast<TChart *>(curr.tC)->pStart;
+				else {
+					stack.push(curr);
+					curr.tC = dynamic_cast<TChart *>(curr.tC)->pStart;
+				}
+			}
+			if (curr.pE == nullptr) {
+				if (dynamic_cast<TChart *>(curr.tC)->pEnd->whatIsIt == 1)
+					curr.pE = dynamic_cast<TChart *>(curr.tC)->pEnd;
+				else {
+
+					stack.push(curr);
+					curr.tC = dynamic_cast<TChart *>(curr.tC)->pEnd;
+					curr.pS = nullptr;
+					stack.push(curr);
+				}
+			}
+			if (curr.pS != nullptr && curr.pE != nullptr)
+				if (curr.pS->whatIsIt == 1 && curr.pE->whatIsIt == 1) {
+					TPoint *p1 = dynamic_cast<TPoint *>(curr.pS), *p2 = dynamic_cast<TPoint *>(curr.pE);
+					if (curr.tC->visible) {
+						TPoint *p1 = dynamic_cast<TPoint *>(curr.pS), *p2 = dynamic_cast<TPoint *>(curr.pE);
+						float x, y, c, d;
+						x = p1->getY() - p2->getY();
+						y = p2->getX() - p1->getX();
+						c = p1->getX()*(p2->getY() - p1->getY()) - p1->getY()*(p2->getX() - p1->getX());
+						d = fabs(x1*x + y1*y + c) / sqrt(x*x + y*y);
+						if (d < Epsilon / 3) {
+							return true;
+
+						}
+					}
+					if (!stack.isEmpty()) {
+						curr = stack.pop();
+						if (curr.pS == nullptr) curr.pS = (TRoot*)p2;
+						else curr.pE = (TRoot*)p2;
+						stack.push(curr);
+					}
+				}
+
+		}
+		return false;
+	}
 	TRoot* Excretion(Graphics^ gr,int x1, int y1) {
 		int Epsilon = 10;
 		TCurrLine curr;
